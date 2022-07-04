92bc359a6229394d5a154eb3dcd8ff113591d062
Alexey Shurygin <git@alexeyshurygin.com>
2022-07-04 03:38:25 +0300

    Perf opt

--- app/src/main/java/WildMatch.java
+++ app/src/main/java/WildMatch.java
@@ -6,6 +6,8 @@ import java.util.List;
  * @author Alexey Shurygin
  */
 public class WildMatch {
+    private int[] count;
+
     enum Kind {
         START, //start
         LETTER,//letter
@@ -15,25 +17,24 @@ public class WildMatch {
     }
 
     public boolean isMatch(String s, String p) {
-//        System.out.print(p);
         do {
             p = p.replace("**", "*");
         } while (p.contains("**"));
-//        System.out.println(" -> " + p);
-        int[] count = new int[p.length()];
+        count = new int[p.length()];
         int sum = 0;
+        var pc = p.toCharArray();
         for (int i = count.length - 1; i >= 0; i--) {
             count[i] = sum;
-            if (p.charAt(i) != '*')
+            if (pc[i] != '*')
                 sum++;
         }
-        return tryAdvance(s, p, Kind.START, (char) 0, 0, 0, count);
+        return tryAdvance(s.toCharArray(), pc, Kind.START, 0, 0);
     }
 
-    Kind getPatternKind(String p, int ip) {
-        if (ip >= p.length())
+    Kind getPatternKind(char[] p, int ip) {
+        if (ip >= p.length)
             return Kind.END;
-        switch (p.charAt(ip)) {
+        switch (p[ip]) {
             case '*':
                 return Kind.SEQ;
             case '?':
@@ -43,33 +44,18 @@ public class WildMatch {
         }
     }
 
-    boolean nextPattern(String s, String p, int ip, int is, int[] count) {
-        final Kind kind = getPatternKind(p, ip);
-        switch (kind) {
-            case START:
-                throw new RuntimeException("START");
-            case END:
-                return tryAdvance(s, p, kind, (char) 0, ip + 1, is, count);
-            case LETTER:
-                return tryAdvance(s, p, kind, p.charAt(ip), ip + 1, is, count);
-            case ANY:
-                return tryAdvance(s, p, kind, (char) 0, ip + 1, is, count);
-            case SEQ:
-                return tryAdvance(s, p, kind, (char) 0, ip + 1, is, count);
-        }
-        throw new RuntimeException("NO");
+    boolean nextPattern(char[] s, char[] p, int ip, int is) {
+        return tryAdvance(s, p, getPatternKind(p, ip), ip + 1, is);
     }
 
     /**
      * Tries one pattern. Returns if that was unsuccessful.
      *
-     * @param kind  - previous kind of pattern
-     * @param l     - letter for SEQ/LETTER
-     * @param ip    - current p pointer
-     * @param is    - current s pointer
-     * @param count
+     * @param kind - previous kind of pattern
+     * @param ip   - current p pointer
+     * @param is   - current s pointer
      */
-    public boolean tryAdvance(String s, String p, Kind kind, char l, int ip, int is, int[] count) {
+    public boolean tryAdvance(char[] s, char[] p, Kind kind, int ip, int is) {
         /*
         start - define 1st pattern and go to the cycle.
         letter and any match to 1 symbol, if not - rollback.
@@ -83,20 +69,22 @@ public class WildMatch {
                 if (ip != 0) throw new IllegalArgumentException("ip=" + ip);
                 kind = getPatternKind(p, ip);
                 if (kind == Kind.END)
-                    return s.isEmpty();
-                return nextPattern(s, p, ip, is, count);
+                    return s.length == 0;
+                return nextPattern(s, p, ip, is);
             case END:
-                return is >= s.length();
+                return is >= s.length;
             case LETTER, ANY:
-                if (is >= s.length() || (kind == Kind.LETTER && s.charAt(is) != l)) return false;
-                return nextPattern(s, p, ip, is + 1, count);
+                if (is >= s.length || (kind == Kind.LETTER && s[is] != p[ip - 1])) return false;
+                return nextPattern(s, p, ip, is + 1);
             case SEQ:
-                if (is >= s.length())
-                    return nextPattern(s, p, ip, is, count);
-                for (int i = s.length() - count[ip - 1]; i >= is; i--) {
+                if (is >= s.length)
+                    return count[ip - 1] == 0;
+                if (count[ip - 1] == 0)
+                    return true;
+                for (int i = s.length - count[ip - 1]; i >= is; i--) {
                     //inc string
                     //inc pattern
-                    if (nextPattern(s, p, ip, i, count)) {
+                    if (nextPattern(s, p, ip, i)) {
                         return true;
                     }
                 }
