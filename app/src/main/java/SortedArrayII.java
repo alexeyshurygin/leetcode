83c379a562f318dd0ef0d46b726171d89a55aaac
Alexey Shurygin <alexeyshurygin@users.noreply.github.com>
2022-07-10 00:25:03 +0300

    Create SortedArrayII.java

---
+++ app/src/main/java/SortedArrayII.java
@@ -0,0 +1,23 @@
+/**
+* Beats 100% of solutions of
+* https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+*/
+public class SortedArrayII {
+public boolean search(final int[] n, final int t) {
+if (n[0] <= t){
+for (int i = 0; i < n.length; i++) {
+if (n[i] == t)
+return true;
+if (n[i] > t)
+return false;
+}} else if (n[n.length - 1] >= t) {
+for (int i = n.length -1; i >= 0; i--) {
+if (n[i] == t)
+return true;
+if (n[i] < t)
+return false;
+}
+}
+return false;
+}
+}
