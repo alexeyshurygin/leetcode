d585ad9e0f58e51a0f84b26612a1f4e9fca2daf0
Alexey Shurygin <git@alexeyshurygin.com>
2022-07-10 17:31:21 +0300

    Fix build

--- app/src/main/java/SearchRotatedArray.java
+++ app/src/main/java/SearchRotatedArray.java
@@ -1,23 +1,24 @@
 /**
-* Beats 100% of solutions of
-* https://leetcode.com/problems/search-in-rotated-sorted-array/description/
-*/
-public class SortedArrayII {
-public int search(final int[] n, final int t) {
-if (n[0] <= t){
-for (int i = 0; i < n.length; i++) {
-if (n[i] == t)
-return i;
-if (n[i] > t)
-return -1;
-}} else if (n[n.length - 1] >= t) {
-for (int i = n.length -1; i >= 0; i--) {
-if (n[i] == t)
-return i;
-if (n[i] < t)
-return -1;
-}
-}
-return -1;
-}
+ * Beats 100% of solutions of
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ */
+public class SearchRotatedArray {
+    public int search(final int[] n, final int t) {
+        if (n[0] <= t) {
+            for (int i = 0; i < n.length; i++) {
+                if (n[i] == t)
+                    return i;
+                if (n[i] > t)
+                    return -1;
+            }
+        } else if (n[n.length - 1] >= t) {
+            for (int i = n.length - 1; i >= 0; i--) {
+                if (n[i] == t)
+                    return i;
+                if (n[i] < t)
+                    return -1;
+            }
+        }
+        return -1;
+    }
 }
