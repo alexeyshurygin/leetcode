62a056bf69f70896d213fa604b0f9f8fb32c9462
Alexey Shurygin <git@alexeyshurygin.com>
2022-08-07 02:46:29 +0300

    Template

--- app/src/main/java/SolutionTemplate.java
+++ app/src/main/java/SolutionTemplate.java
@@ -6,6 +6,7 @@ import java.util.*;
  */
 public class SolutionTemplate {
     public static void main(String[] args) {
-
+        SolutionTemplate s = new SolutionTemplate();
+//        System.out.println();
     }
 }
