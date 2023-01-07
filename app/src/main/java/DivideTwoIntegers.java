1b060af6f01723a3a9467c9802e2cf59658bfec1
Alexey Shurygin <git@alexeyshurygin.com>
2023-01-07 07:56:36 +0300

    Works DivideTwoIntegers

--- app/src/main/java/DivideTwoIntegers.java
+++ app/src/main/java/DivideTwoIntegers.java
@@ -5,6 +5,9 @@
  */
 public class DivideTwoIntegers {
     public int divide(int dividend, int divisor) {
-        return dividend / divisor;
+        long result = (long) dividend / divisor;
+        result = Math.min(Integer.MAX_VALUE, result);
+        result = Math.max(Integer.MIN_VALUE, result);
+        return (int) result;
     }
 }
