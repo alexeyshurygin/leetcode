ba61ad6f3a6854173b515746819822020d200751
Alexey Shurygin <git@alexeyshurygin.com>
2023-01-09 04:25:27 +0300

    In progress StrongPasswordChecker

--- app/src/main/java/StrongPasswordChecker.java
+++ app/src/main/java/StrongPasswordChecker.java
@@ -1,96 +1,343 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
+ * https://leetcode.com/problems/strong-password-checker
+ *
  * @author Alexey Shurygin
  */
 public class StrongPasswordChecker {
-    private int getReplaceNum(String p) {
-        int r = 0;
-        long l = p.chars().filter(Character::isLowerCase).count();
-        long u = p.chars().filter(Character::isUpperCase).count();
-        long d = p.chars().filter(Character::isDigit).count();
-        char prev = 0;
-        int cnt = 0;
-        int excessCnt = 0;
-        for (int i = 0; i < p.length(); i++) {
-            char c = p.charAt(i);
-            if (c == prev) {
-                cnt++;
-            } else {
-                excessCnt += cnt / 3;
-                cnt = 1;
-                prev = c;
-            }
-        }
-        excessCnt += cnt / 3;
-        System.out.printf("Password:%s, excessCnt:%d\n", p, excessCnt);
-        if (u < 1) r++;
-        if (l < 1) r++;
-        if (d < 1) r++;
-        r = Math.max(r, excessCnt);
-        return r;
+    private static char getDigit(int... chars) {
+        if (Arrays.stream(chars).noneMatch(c -> c == '1'))
+            return '1';
+        if (Arrays.stream(chars).noneMatch(c -> c == '2'))
+            return '2';
+        if (Arrays.stream(chars).noneMatch(c -> c == '3'))
+            return '3';
+        throw new AssertionError();
     }
 
-    private int getReplaceNumEx(String p, int start) {
-        int delN = p.length() - start - 20;
-        if (delN < 0)
-            throw new AssertionError("delN < 0");
-        var sb = new StringBuilder(p.substring(start));
-        int r = 0;
+    private static char getUpper(int... chars) {
+        if (Arrays.stream(chars).noneMatch(c -> c == 'A'))
+            return 'A';
+        if (Arrays.stream(chars).noneMatch(c -> c == 'B'))
+            return 'B';
+        if (Arrays.stream(chars).noneMatch(c -> c == 'C'))
+            return 'C';
+        throw new AssertionError();
+    }
+
+    private static char getLower(int... chars) {
+        if (Arrays.stream(chars).noneMatch(c -> c == 'a'))
+            return 'a';
+        if (Arrays.stream(chars).noneMatch(c -> c == 'b'))
+            return 'b';
+        if (Arrays.stream(chars).noneMatch(c -> c == 'c'))
+            return 'c';
+        throw new AssertionError();
+    }
+
+    boolean strong(StringBuilder p) {
+        if (!(p.length() >= 6 && p.length() <= 20 && p.chars().anyMatch(Character::isLowerCase) && p.chars().anyMatch(Character::isUpperCase) && p.chars().anyMatch(Character::isDigit)))
+            return false;
+        if (getNextChain(p, 0).length > 0) return false;
+        return true;
+    }
+
+    private C getNextChain(StringBuilder p, int start) {
         char prev = 0;
         int cnt = 0;
-        int excessCnt = 0;
-        int fullThreeCnt = 0;
-        for (int i = 0; i < 20; ) {
-            char c = sb.charAt(i);
+        char c = 0;
+        for (int i = start; i < p.length(); i++) {
+            c = p.charAt(i);
             if (c == prev) {
                 cnt++;
-                i++;
             } else {
-                excessCnt += cnt / 3;
-                if (cnt >= 3 && cnt % 3 == 0) {
-                    fullThreeCnt++;
-                    sb.deleteCharAt(i - 1);
-                } else {
-                    i++;
-                }
+                if (cnt >= 3)
+                    return new C(prev, i - cnt, cnt);
                 cnt = 1;
                 prev = c;
             }
         }
-        if (sb.length() > 20)
-            sb.delete(20, sb.length());
-        excessCnt += cnt / 3;
-        if (cnt >= 3 && cnt % 3 == 0) fullThreeCnt++;
-        int toDelete = Math.min(delN, fullThreeCnt);
-        delN -= toDelete;
-        excessCnt -= toDelete;
-        if (excessCnt < 0)
-            throw new AssertionError("excessCnt < 0");
-        if (delN < 0)
-            throw new AssertionError("delN < 0");
-        long l = sb.chars().filter(Character::isLowerCase).count();
-        long u = sb.chars().filter(Character::isUpperCase).count();
-        long d = sb.chars().filter(Character::isDigit).count();
-        if (u < 1) r++;
-        if (l < 1) r++;
-        if (d < 1) r++;
-        r = Math.max(r, excessCnt) + delN;
-        System.out.printf("Password:%s sb:%s delN:%d, excessCnt:%d, r:%d\n", p, sb, delN, excessCnt, r);
-        return r + start;
+        if (cnt < 3) return new C(-1, 0);
+        return new C(c, p.length() - cnt, cnt);
+    }
+
+    public int bfNum(StringBuilder p) {
+        System.out.printf("Initial pw:%s\n", p);
+//      If less than 6: add 6-length chars
+//      add only missing characters
+//      add char/add missing char is the same
+//      If more than 20: remove length-20 chars
+//      remove breaking 3+ first
+//      remove by not creating 3+
+//      remove/break 3+ is the same
+//      remove only non-missing chars
+//      if there's 6<=length<=20 chars check for l u d if there's no - change/add
+//      if there's more than 3 chars - change every 3rd char to another char
+//      break 3+/add missing char is the same
+        if (strong(p)) return 0;
+        int nl = (int) p.chars().filter(Character::isLowerCase).count();
+        int nu = (int) p.chars().filter(Character::isUpperCase).count();
+        int nd = (int) p.chars().filter(Character::isDigit).count();
+        D d = new D(nl, nu, nd);
+        d = breakChains(d, p);
+        while (p.length() > 20) {
+            d = removeChars(d, p);
+        }
+        while (p.length() < 6) {
+            d = addChar(d, p);
+        }
+        d = addMissing(d, p);
+        System.out.printf("Result  pw:%s complexity:%d\n", p, d.n);
+        return d.n;
+    }
+
+    D addChar(D d, StringBuilder p) {
+        assert p.length() < 6;
+        if (d.nl == 0) {
+            p.append("a");
+            return new D(d.n + 1, d.nl + 1, d.nu, d.nd);
+        }
+        if (d.nu == 0) {
+            p.append("A");
+            return new D(d.n + 1, d.nl, d.nu + 1, d.nd);
+        }
+        if (d.nd == 0) {
+            p.append("1");
+            return new D(d.n + 1, d.nl, d.nu, d.nd + 1);
+        }
+        char c = p.charAt(p.length() - 1);
+        p.append(++c);
+        return new D(d.n + 1, d.nl, d.nu, d.nd);
+
+    }
+
+    D removeChars(D d, StringBuilder p) {
+        int n = d.n;
+        int nl = d.nl;
+        int nu = d.nu;
+        int nd = d.nd;
+        for (int i = p.length() - 1; p.length() > 20 && i >= 0; i--) {
+            char ch = p.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                if (nu > 1) {
+                    p.deleteCharAt(i);
+                    nu--;
+                    n++;
+                }
+            } else if (Character.isLowerCase(ch)) {
+                if (nl > 1) {
+                    p.deleteCharAt(i);
+                    nl--;
+                    n++;
+                }
+            } else if (Character.isDigit(ch)) {
+                if (nd > 1) {
+                    p.deleteCharAt(i);
+                    nd--;
+                    n++;
+                }
+            } else {
+                p.deleteCharAt(i);
+                n++;
+            }
+        }
+        return new D(n, nl, nu, nd);
+    }
+
+    D breakChains(D d, StringBuilder p) {
+        int n = d.n;
+        int nl = d.nl;
+        int nu = d.nu;
+        int nd = d.nd;
+        if (p.length() > 20) {
+            // delete from chains
+            C c;
+            do {
+                C maxC;
+                for (maxC = c = getNextChain(p, 0); p.length() > 20 && c.length > 0; c = getNextChain(p, c.index + c.length - 1)) {
+                    if (c.length % 3 != 0) {
+                        if (maxC.length < c.length)
+                            maxC = c;
+                        continue;
+                    }
+                    if (Character.isLowerCase(c.ch))
+                        nl--;
+                    else if (Character.isUpperCase(c.ch))
+                        nu--;
+                    else if (Character.isDigit(c.ch))
+                        nd--;
+                    p.deleteCharAt(c.index);
+                    n++;
+                    if (maxC.length < c.length - 1)
+                        maxC = new C(c.ch, c.index, c.length - 1);
+                }
+                if (p.length() > 20 && maxC.length > 0) {
+                    if (Character.isLowerCase(maxC.ch))
+                        nl--;
+                    else if (Character.isUpperCase(maxC.ch))
+                        nu--;
+                    else if (Character.isDigit(maxC.ch))
+                        nd--;
+                    p.deleteCharAt(maxC.index);
+                    n++;
+                }
+                c = getNextChain(p, 0);
+            } while (p.length() > 20 && c.length > 0);
+        }
+//        aaaaabbbb1234567890ABA
+//        aa1bb1b1234567890ABA
+//        aa1aabb1234567890ABA
+//        aa1bb1b1234567890ABA
+        if (p.length() <= 20 && p.length() >= 6) {
+            // change chars
+            // avoid creating new chains by changing to equal adjacent character
+            for (C c = getNextChain(p, 0); c.length > 0; c = getNextChain(p, c.index + c.length)) {
+                for (int i = c.index + 2; i < c.index + c.length; i += 3) {
+                    char ch;
+                    if (nl == 0) {
+                        ch = getLower(c.ch);
+                        nl++;
+                    } else if (nu == 0) {
+                        ch = getUpper(c.ch);
+                        nu++;
+                    } else/* if (nd == 0) */ {
+                        ch = getDigit(c.ch);
+                        nd++;
+                    }
+                    p.setCharAt(i, ch);
+                    n++;
+                }
+            }
+        }
+        if (p.length() < 6) {
+            // Add chars
+            // avoid creating new chains by changing to equal adjacent character
+            for (C c = getNextChain(p, 0); c.length > 0; c = getNextChain(p, c.index + c.length)) {
+                for (int i = c.index + 2; i < c.index + c.length; i += 3) {
+                    char ch;
+                    if (nl == 0) {
+                        ch = getLower(c.ch);
+                        nl++;
+                    } else if (nu == 0) {
+                        ch = getUpper(c.ch);
+                        nu++;
+                    } else/* if (nd == 0) */ {
+                        ch = getDigit(c.ch);
+                        nd++;
+                    }
+                    p.insert(i, ch);
+                    n++;
+                }
+            }
+        }
+        return new D(n, nl, nu, nd);
+    }
+
+    D addMissing(D d, StringBuilder p) {
+        assert p.length() >= 6 && p.length() <= 20;
+        if (d.nl > 0 && d.nu > 0 && d.nd > 0) {
+            return d;
+        }
+        int n = d.n;
+        int nl = d.nl;
+        int nu = d.nu;
+        int nd = d.nd;
+        if (nl == 0) {
+            if (p.length() < 20) {
+                p.append(getLower(p.charAt(p.length() - 1)));
+            } else {
+                for (int i = 0; i < p.length(); i++) {
+                    char ch = p.charAt(i);
+                    if (nu > 1 && Character.isUpperCase(ch) || nd > 1 && Character.isDigit(ch)) {
+                        var chars = new ArrayList<Integer>();
+                        chars.add((int) ch);
+                        if (i > 0)
+                            chars.add((int) p.charAt(i - 1));
+                        if (i < p.length() - 1)
+                            chars.add((int) p.charAt(i + 1));
+                        char c = getLower(chars.stream().mapToInt(x -> x).toArray());
+                        if (Character.isUpperCase(ch))
+                            nu--;
+                        if (Character.isDigit(ch))
+                            nd--;
+                        p.setCharAt(i, c);
+                        break;
+                    }
+                }
+            }
+            nl++;
+            n++;
+        }
+        if (nu == 0) {
+            if (p.length() < 20) {
+                p.append(getUpper(p.charAt(p.length() - 1)));
+            } else {
+                for (int i = 0; i < p.length(); i++) {
+                    char ch = p.charAt(i);
+                    if (nl > 1 && Character.isLowerCase(ch) || nd > 1 && Character.isDigit(ch)) {
+                        var chars = new ArrayList<Integer>();
+                        chars.add((int) ch);
+                        if (i > 0)
+                            chars.add((int) p.charAt(i - 1));
+                        if (i < p.length() - 1)
+                            chars.add((int) p.charAt(i + 1));
+                        char c = getUpper(chars.stream().mapToInt(x -> x).toArray());
+                        if (Character.isLowerCase(ch))
+                            nl--;
+                        if (Character.isDigit(ch))
+                            nd--;
+                        p.setCharAt(i, c);
+                        break;
+                    }
+                }
+            }
+            nu++;
+            n++;
+        }
+        if (nd == 0) {
+            if (p.length() < 20) {
+                p.append(getDigit(p.charAt(p.length() - 1)));
+            } else {
+                for (int i = 0; i < p.length(); i++) {
+                    char ch = p.charAt(i);
+                    if (nl > 1 && Character.isLowerCase(ch) || nu > 1 && Character.isUpperCase(ch)) {
+                        var chars = new ArrayList<Integer>();
+                        chars.add((int) ch);
+                        if (i > 0)
+                            chars.add((int) p.charAt(i - 1));
+                        if (i < p.length() - 1)
+                            chars.add((int) p.charAt(i + 1));
+                        char c = getLower(chars.stream().mapToInt(x -> x).toArray());
+                        if (Character.isLowerCase(ch))
+                            nl--;
+                        if (Character.isUpperCase(ch))
+                            nu--;
+                        p.setCharAt(i, c);
+                        break;
+                    }
+                }
+            }
+            nd++;
+            n++;
+        }
+        return new D(n, nl, nu, nd);
     }
 
     public int strongPasswordChecker(String p) {
-        int min = 0;
-        if (p.length() < 6) min = 6 - p.length();
-        if (p.length() > 20) {
-            int minR = Integer.MAX_VALUE;
-            for (int l = 0; l <= p.length() - 20; l++) {
-                int r = getReplaceNumEx(p, l);
-                minR = Math.min(minR, r);
-            }
-            return minR;
+        return bfNum(new StringBuilder(p));
+    }
+
+    record C(char ch, int index, int length) {
+        C(int index, int length) {
+            this((char) 0, index, length);
         }
-        int r = getReplaceNum(p);
-        r = Math.max(r, min);
-        return r;
+    }
+
+    record D(int n, int nl, int nu, int nd) {
+        D(int nl, int nu, int nd) {
+            this(0, nl, nu, nd);
+        }
     }
 }
--- app/src/test/java/StrongPasswordCheckerTest.java
+++ app/src/test/java/StrongPasswordCheckerTest.java
@@ -16,11 +16,15 @@ class StrongPasswordCheckerTest {
 
     @Test
     void strongPasswordChecker() {
-        assertEquals(5, i.strongPasswordChecker("a"));
-        assertEquals(3, i.strongPasswordChecker("aA1"));
-        assertEquals(0, i.strongPasswordChecker("1337C0d3"));
-        assertEquals(2, i.strongPasswordChecker("aaa111"));
-        assertEquals(2, i.strongPasswordChecker("ABABABABABABABABABAB1"));
-        assertEquals(8, i.strongPasswordChecker("bbaaaaaaaaaaaaaaacccccc"));
+//        assertEquals(5, i.strongPasswordChecker("a"));
+//        assertEquals(3, i.strongPasswordChecker("aA1"));
+//        assertEquals(0, i.strongPasswordChecker("1337C0d3"));
+//        assertEquals(2, i.strongPasswordChecker("aaa111"));
+//        assertEquals(2, i.strongPasswordChecker("ABABABABABABABABABAB1"));
+//        assertEquals(8, i.strongPasswordChecker("bbaaaaaaaaaaaaaaacccccc"));
+//        assertEquals(1, i.strongPasswordChecker("aaaB1"));
+//        assertEquals(8, i.strongPasswordChecker("aaaabbbbccccddeeddeeddeedd"));
+//        assertEquals(3, i.strongPasswordChecker("aaaaabbbb1234567890ABA"));
+        assertEquals(4, i.strongPasswordChecker("A1234567890aaabbbbccccc"));
     }
 }
