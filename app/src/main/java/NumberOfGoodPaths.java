8ef9cc9e0943417b9295863fe32700f78ac7c111
Alexey Shurygin <git@alexeyshurygin.com>
2023-01-16 04:32:47 +0300

    Kinda works. Slow

--- app/src/main/java/NumberOfGoodPaths.java
+++ app/src/main/java/NumberOfGoodPaths.java
@@ -7,11 +7,11 @@ import java.util.function.Predicate;
  * @author Alexey Shurygin
  */
 public class NumberOfGoodPaths {
-    static int cacheHit;
     static int calls;
+    static int iters;
 
     public int numberOfGoodPaths(int[] vals, int[][] edges) {
-        System.out.printf("Mem:%d\n", Runtime.getRuntime().maxMemory());
+//        System.out.printf("Mem:%d\n", Runtime.getRuntime().maxMemory());
         Objects.requireNonNull(vals, "vals null");
         Objects.requireNonNull(edges, "edges null");
         int r = vals.length;
@@ -22,62 +22,50 @@ public class NumberOfGoodPaths {
             edgeMap.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
             edgeMap.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
         }
-        var cache = new HashMap<CacheKey, Integer>();
         for (List<Integer> nodes : val2node.values()) {
             if (nodes.size() >= 2) {
                 //handle it
                 for (int i = 0; i < nodes.size() - 1; i++) {
                     for (int j = i + 1; j < nodes.size(); j++) {
                         Integer start = nodes.get(i);
-                        int maxVal = findPaths(start, nodes.get(j), vals, edgeMap, cache);
-                        if (maxVal >= 0 && maxVal <= vals[start])
+                        int maxVal = findPaths(start, nodes.get(j), vals, edgeMap, vals[start]);
+                        if (maxVal >= 0 && maxVal <= vals[start]) {
                             r++;
+                            System.out.printf("Calls:%d, iterations:%d\n", calls, iters);
+                        }
                     }
                 }
-//                System.out.printf("Cache hits:%d, #calls:%d, cache:%d\n", cacheHit, calls, cache.size());
             }
         }
         return r;
     }
 
-    int findPaths(int start, int end, int[] vals, Map<Integer, List<Integer>> edgeMap, HashMap<CacheKey, Integer> cache) {
+    int findPaths(int start, int end, int[] vals, Map<Integer, List<Integer>> edgeMap, int max) {
         calls++;
         //BFS
         var q = new LinkedHashMap<Integer, Integer>();
-        var walked = new HashSet<Integer>();
+        var walked = new LinkedHashSet<Integer>();
         q.put(start, vals[start]);
+        walked.add(start);
         while (!q.isEmpty()) {
+            iters++;
             Integer node = q.keySet().iterator().next();
             final Integer nodeMax = q.remove(node);
             assert nodeMax >= 0;
-//            cache.putIfAbsent(new CacheKey(start, node), nodeMax);
+            assert walked.contains(node);
             if (end == node)
                 return nodeMax;
-            Integer restVal = cache.get(new CacheKey(node, end));
-            if (restVal != null) {
-                cacheHit++;
-                int val = restVal < 0 ? restVal : Math.max(nodeMax, restVal);
-                cache.putIfAbsent(new CacheKey(start, end), val);
-                return val;
-            }
-            walked.add(node);
+//            if (nodeMax > max) {
+//                walked.addAll(edgeMap.getOrDefault(node, List.of()));
+//                continue;
+//            }
             //defensive
-            edgeMap.getOrDefault(node, List.of()).stream().filter(Predicate.not(walked::contains)).forEach(n -> q.put(n, Math.max(nodeMax, vals[n])));
+            edgeMap.getOrDefault(node, List.of()).stream().filter(Predicate.not(walked::contains)).forEach(n -> {
+                q.put(n, Math.max(nodeMax, vals[n]));
+                walked.add(n);
+            });
+//            .filter(n -> vals[n] <= max)
         }
-        cache.putIfAbsent(new CacheKey(start, end), -1);
         return -1;
-    }
-
-    record CacheKey(int start, int end) {
-        CacheKey(int start, int end) {
-            if (start <= end) {
-                this.start = start;
-                this.end = end;
-            } else {
-                this.start = end;
-                this.end = start;
-            }
-            assert this.start <= this.end;
-        }
     }
 }
